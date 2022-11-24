86b9a56f3c31716d845aede4d393ddd6cf171505
BenLiu <liumanpan1990@hotmail.com>
2022-11-24 11:11:06 -0500

    bug fix

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { pluralize } from '../../utils/helpers';
-import { useStoreContext } from '../../utils/GlobalState';
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+
 import { idbPromise } from '../../utils/helpers';
 import { useSelector, useDispatch } from 'react-redux';
 import { addNewToCart, updateCarQuantity } from '../../utils/shopSlice';
 
 function ProductItem(item) {
-  // const [state, dispatch] = useStoreContext();
   const state = useSelector((state) => state.shop);
   const dispatch = useDispatch();
-  // console.log(state);
 
   const { image, name, _id, price, quantity } = item;
 
